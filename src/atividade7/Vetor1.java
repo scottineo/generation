445567449f029c870df275ee5a37b8e796c4dfff
Henrique Machado <henriquesousa@ymail.com>
2025-03-21 09:35:29 -0300

    formatação

--- src/atividade7/Vetor1.java
+++ src/atividade7/Vetor1.java
@@ -3,26 +3,26 @@ package atividade7;
 import java.util.Scanner;
 
 public class Vetor1 {
-public static void main(String[] args) {
-    int[] vetor = {2,5,1,3,4,9,7,8,10,6};
-    int numeroInput;
-    boolean encontrado = false;
-    Scanner leia = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] vetor = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+        int numeroInput;
+        boolean encontrado = false;
+        Scanner leia = new Scanner(System.in);
 
-    System.out.println("Digite um número: ");
-    numeroInput = leia.nextInt();
+        System.out.println("Digite um número: ");
+        numeroInput = leia.nextInt();
 
-    for (int i = 0; i < vetor.length; i++) {
-        if (vetor[i] == numeroInput) {
-            System.out.println("O número " + numeroInput + " está na posição " + i + " do vetor.");
-            encontrado = true;
-            break;
-        } 
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numeroInput) {
+                System.out.println("O número " + numeroInput + " está na posição " + i + " do vetor.");
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("O número " + numeroInput + " não está no vetor.");
+        }
+        leia.close();
+
     }
-    if (!encontrado) {
-        System.out.println("O número " + numeroInput + " não está no vetor.");
-    }
-    leia.close();
-   
-}
 }
